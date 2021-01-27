aa99be6d37345c31c8c96437fab100f7071a9a78
witold <wiltold.wroblewski@sennder.com>
2021-01-27 18:40:30 +0100

    add audierne immobilier

--- sizun.py
+++ sizun.py
@@ -70,6 +70,13 @@ t_audierne_immobilier = BashOperator(
     trigger_rule='all_done'
 )
 
+t_bourse_immobilier = BashOperator(
+    task_id='bourse_immobilier',
+    bash_command='/root/sizun/venv/bin/python /root/sizun/scrapper.py bourse_immobilier',
+    dag=dag,
+    trigger_rule='all_done'
+)
+
 t_check_and_build = BashOperator(
     task_id='check_and_build',
     bash_command='/root/sizun/venv/bin/python /root/sizun/check_changes.py "{{dag_run.start_date}}"',
@@ -78,4 +85,4 @@ t_check_and_build = BashOperator(
 )
 
 
-t_ouest_france >> t_immonot >> t_figaro >> t_superimmo >>  t_planete_immobilier >> t_audierne_immobilier >> t_check_and_build
+t_ouest_france >> t_immonot >> t_figaro >> t_superimmo >>  t_planete_immobilier >> t_audierne_immobilier >> t_bourse_immobilier >> t_check_and_build
